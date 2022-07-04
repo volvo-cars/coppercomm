e8e769ca1bf86bec34a8fd8e7035f45bd29ee042
Daniel Bober <daniel@haleytek.com>
2022-07-05 01:46:46 +0200

    Add adb logger fixture for whole session
    - Allow the user to get a single logcat file from test session
    
    HT-9072
    
    Change-Id: I0ff8663af76804c4c4068ed6400b01c737d797ad

--- src/coppercomm/loggers/pytest_fixtures/log_fixtures.py
+++ src/coppercomm/loggers/pytest_fixtures/log_fixtures.py
@@ -10,17 +10,31 @@ from coppercomm.loggers.utils.log_dir import LogDir
 def log_dir(request) -> typing.Iterator[LogDir]:
     dir_name = request.function.__name__
     log_dir = LogDir(dir_name)
-    yield log_dir
+    return log_dir
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def log_dir_session() -> typing.Iterator[LogDir]:
+    dir_name = "session"
+    log_dir = LogDir(dir_name)
+    return log_dir
+
+
+@pytest.fixture(scope="function")
 def adb_logcat_logger(adb, log_dir):
     logger = DeviceLoggerViaAdb(adb.device_id, "logcat", test_log_dir=log_dir.name, shell=True)
     with logger:
         yield
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def adb_logcat_logger_session(adb, log_dir_session):
+    logger = DeviceLoggerViaAdb(adb.device_id, "logcat", test_log_dir=log_dir_session.name, shell=True)
+    with logger:
+        yield
+
+
+@pytest.fixture(scope="function")
 def adb_dmesg_logger(adb, log_dir):
     logger = DeviceLoggerViaAdb(adb.device_id, "dmesg -w", test_log_dir=log_dir.name, shell=True)
     with logger:
