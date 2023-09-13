aa5718884d59c673576510c0ca3554d5d3c5a912
Michal Szczepanski <michal@haleytek.com>
2023-09-13 09:45:17 +0200

    Fix: Pass qnx_serial to conditional serial logging function instead of DeviceFactory.
    We need to reuse existing qnx_serial instance instad of creating a new one, to prevent
    errors like:
    
    Connection address /dev/ttyUSB0 is used by process with PID: 1
    
    HT-22776
    
    Change-Id: I07ea237d8dbd1d85aa224e99e25eb1952435ff24

--- src/coppercomm/loggers/serial_logger.py
+++ src/coppercomm/loggers/serial_logger.py
@@ -13,36 +13,21 @@ from __future__ import annotations
 import typing
 from contextlib import contextmanager
 
-from coppercomm.config_file_parser import SerialDeviceType
-
 if typing.TYPE_CHECKING:
     from pathlib import Path
 
-    from coppercomm.device_factory import DeviceFactory
     from coppercomm.device_serial.device_serial import SerialConnection
 
 
-def get_qnx_serial_logger(device: DeviceFactory, log_path: Path) -> SerialConnection:
-    """Get SerialConnection object for QNX with enabled logging
-
-    :param device: device factory
-    :param log_path: path to log file
-    :return: serial connection object for QNX with logging
-    """
-    qnx_serial = device.create_serial(SerialDeviceType.QNX)
-    qnx_serial.set_test_logging(log_path)
-    return qnx_serial
-
-
 @contextmanager
-def conditional_qnx_serial_logging(device: DeviceFactory, log_path: typing.Optional[Path]):
+def conditional_qnx_serial_logging(qnx_serial: typing.Optional[SerialConnection], log_path: typing.Optional[Path]):
     """Enter QNX serial logging context only if log path is provided
 
     :param device: device factory
     :param log_path: path to log file
     """
-    if log_path:
-        with get_qnx_serial_logger(device, log_path):
-            yield
+    if qnx_serial and log_path:
+        qnx_serial.set_test_logging(log_path)
+        yield
     else:
         yield
